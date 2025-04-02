a9d271095c3a866872071f86ca578913a7c1cc6a
EduardoMVAz <emendesvaz@gmail.com>
2025-04-02 02:44:23 +0000

    Changes on the dockefile

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -5,10 +5,3 @@ echo "Setting up environment..."
 opam init -y
 opam switch create 4.14.0
 eval $(opam env)
-
-# Update Cabal and GHCup paths
-echo 'export PATH="$HOME/.ghcup/bin:$HOME/.cabal/bin:$HOME/.local/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
-
-cabal update
-cabal install hlint stylish-haskell
